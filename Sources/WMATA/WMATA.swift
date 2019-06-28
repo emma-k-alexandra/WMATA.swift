839138d3388a2bb628086e9b219390c218b26558
Emma Foster <emma@emma.sh>
2019-06-27 22:14:53 -0400

    WMATA publicizing

--- Sources/WMATA/WMATA.swift
+++ Sources/WMATA/WMATA.swift
@@ -9,19 +9,19 @@ import Foundation
 /// Main way of interacting with the WMATA API
 public class WMATA {
     /// WMATA API key from dev portal
-    var apiKey: String
+    public var apiKey: String
     
     /// URLSession to use for all requests
-    var session: URLSession
+    public var session: URLSession
     
     /// General MetroRail information
-    lazy var rail: Rail = {
+    public lazy var rail: Rail = {
         return Rail(apiKey: self.apiKey)
         
     }()
     
     /// General MetroBus information
-    lazy var bus: Bus = {
+    public lazy var bus: Bus = {
         return Bus(apiKey: self.apiKey)
         
     }()
@@ -30,32 +30,32 @@ public class WMATA {
     ///
     /// - parameter apiKey: WMATA API key from dev portal
     /// - parameter session: Session to call on requests on
-    init(apiKey: String, session: URLSession = URLSession.shared) {
+    public init(apiKey: String, session: URLSession = URLSession.shared) {
         self.apiKey = apiKey
         self.session = session
         
     }
     
     /// Subscript support for Stations
-    subscript(index: Station.Code) -> Station {
+    public subscript(index: Station.Code) -> Station {
         return Station(apiKey: self.apiKey, code: index)
         
     }
     
     /// Subscript support for Line
-    subscript(index: Line.Code) -> Line {
+    public subscript(index: Line.Code) -> Line {
         return Line(apiKey: self.apiKey, line: index)
         
     }
     
     /// Subscript support for Stops
-    subscript(index: String) -> Stop {
+    public subscript(index: String) -> Stop {
         return Stop(apiKey: self.apiKey, stopId: index)
         
     }
     
     /// Subscript support for Routes
-    subscript(index: Route.Id) -> Route {
+    public subscript(index: Route.Id) -> Route {
         return Route(apiKey: self.apiKey, routeId: index)
         
     }
