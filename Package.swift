8bcf90681ed78626d1e64c761ba73733ef5c9e86
Emma K Alexandra <emma@emma.sh>
2022-07-06 19:54:26 -0400

    docs: add github pages site

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -17,21 +17,23 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            name: "SwiftProtobuf",
             url: "https://github.com/apple/swift-protobuf.git",
             .upToNextMajor(from: .init(1, 18, 0))
         ),
         .package(
-            name: "DVR",
             url: "https://github.com/venmo/DVR.git",
             .upToNextMajor(from: .init(2, 0, 0))
         ),
+        .package(
+            url: "https://github.com/apple/swift-docc-plugin",
+            from: "1.0.0"
+        )
     ],
     targets: [
         .target(
             name: "WMATA",
             dependencies: [
-                "SwiftProtobuf"
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
             ]
         ),
         .testTarget(
